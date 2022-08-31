3932a43b98583c9c583fb2aeea36749384c58d7a
Minh Buu <minhbuu@MacBook-Air-de-Minh.local>
2022-08-31 19:19:34 +0200

    juste pour heroku

--- wp-config.php
+++ wp-config.php
@@ -20,37 +20,34 @@
 
 /**  on est dans heroku */
 
-if (getenv('JAWSDB_URL')!==false){
-	$dbparts = parse_url($url);
-	
-	$hostname = $dbparts['host'];
-	$username = $dbparts['user'];
-	$password = $dbparts['pass'];
-	$database = ltrim($dbparts['path'],'/');
-	} else {
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
 
-
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'blinkedsolutions' );
+/**define( 'DB_NAME', 'blinkedsolutions' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+/**define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+/**define( 'DB_PASSWORD', 'root' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost:8889' );
+/**define( 'DB_HOST', 'localhost:8889' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+/**define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+/**define( 'DB_COLLATE', '' );
 
-}
+
 
 /**#@+
  * Authentication unique keys and salts.
